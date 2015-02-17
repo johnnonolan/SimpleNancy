96ea3a40f2903c55c43f8fd26c3f5dd1f655300d
John Nolan <jnolan@opentable.com>
2015-02-17 10:49:42 +0000

    :hatching_chick: initial commit

---
+++ SimpleNancy.SelfHost/HelloModule.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleNancy.SelfHost
+{
+	using Nancy;
+
+	public class HelloModule : NancyModule
+	{
+		public HelloModule()
+		{
+			Get["/"] = _ => "Hello I'm a self hosting Nancy process hopwfully from inside a docker container running on mono!";
+		}
+
+	}
+}
---
+++ SimpleNancy.SelfHost/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace SimpleNancy.SelfHost
+{
+	class Program
+	{
+		static void Main()
+		{
+			using (var host = new NancyHost(new Uri("http://localhost:1234")))
+			{
+				host.Start();
+				Console.ReadLine();
+			}
+
+		}
+	}
+}
